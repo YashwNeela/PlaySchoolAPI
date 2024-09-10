1b1de0c79e0ce342f487e8de2305288381ac9c85
YashwNeela <yashw@neelafilm.com>
2024-09-10 17:20:40 +0530

    Hello

--- Scripts/PlayschoolTestData.cs
+++ Scripts/PlayschoolTestData.cs
@@ -33,7 +33,6 @@ public class PlayschoolTestData
     public PlayschoolTestData(int maxTextQuestions)
     {
         m_MaxTestQuestions = maxTextQuestions;
-        SetPlaySchoolTestStars();
     }
 
     ~PlayschoolTestData()
@@ -42,19 +41,9 @@ public class PlayschoolTestData
     }
 
     #region Stars
-
-    private void SetPlaySchoolTestStars()
-    {
-        m_PlaySchoolTestStars = new List<PlaySchoolTestStars>();
-
-        for (int i = 0; i < PlaySchoolTestDataConstants.MAX_STARS_ATTEMPT; i++)
-        {
-            m_PlaySchoolTestStars.Add(new PlaySchoolTestStars(i, PlaySchoolTestDataConstants.FIRST_ATTEMPT_MAX_STARS - i));
-        }
-    }
-
     public int GetStarsBasedOnAttempt(int attemptNumber, int questionRight)
     {
+        //HEELO TESTING
         float percentage = ((float)questionRight / m_MaxTestQuestions) * 100;
 
         switch (attemptNumber)
@@ -72,7 +61,7 @@ public class PlayschoolTestData
                     return 0; // Less than 60% correct answers
 
             case 2:
-             if (percentage >= 100)
+                if (percentage >= 100)
                     return 4; // 100% correct answers
                 else if (percentage >= 80)
                     return 3; // 80% to 99% correct answers
@@ -82,8 +71,9 @@ public class PlayschoolTestData
                     return 0; // 60% to 69% correct answers
                 else
                     return 0; // Less than 60% correct answers
-            case 3:
-             if (percentage >= 100)
+
+            default:
+                if (percentage >= 100)
                     return 3; // 100% correct answers
                 else if (percentage >= 80)
                     return 2; // 80% to 99% correct answers
@@ -93,9 +83,8 @@ public class PlayschoolTestData
                     return 0; // 60% to 69% correct answers
                 else
                     return 0; // Less than 60% correct answers
-        }
 
-        return -1;
+        }
     }
 
 
