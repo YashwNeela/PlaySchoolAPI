8be22cb11d4de416962717df21b1d05fecc1c6e3
YashwNeela <yashw@neelafilm.com>
2024-09-28 17:35:58 +0530

    Playschool shell script made

---
+++ ShellScripts/PlaySchoolAPI/PlaySchoolAPI.sh
@@ -0,0 +1,9 @@
+cd Assets
+cd Colorful-Crayons
+git reset --hard main
+git config core.sparseCheckout true
+echo Assets/_SortingGame/_Scripts/ > .git/info/sparse-checkout
+git fetch origin
+git pull origin main
+git read-tree -mu HEAD
+
