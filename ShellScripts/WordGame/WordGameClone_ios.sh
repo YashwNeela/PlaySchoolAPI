0542aa58350e185e3e12bd688cf28b4a7a55bac1
YashwNeela <yashw@neelafilm.com>
2024-09-30 18:23:45 +0530

    Delete ShellScripts/WordGame directory

--- ShellScripts/WordGame/WordGameClone_ios.sh
+++
@@ -1,7 +0,0 @@
-cd Assets
-git clone --recurse-submodules https://github.com/TMKOC-Play-School/WordGame
-cd WordGame
-git config core.sparseCheckout true
-echo Assets/Scripts/ >> .git/info/sparse-checkout
-git read-tree -mu HEAD
-
--- ShellScripts/WordGame/WordGamePull_ios.sh
+++
@@ -1,9 +0,0 @@
-cd Assets
-cd WordGame
-git reset --hard main
-git config core.sparseCheckout true
-echo Assets/Scripts/ > .git/info/sparse-checkout
-git fetch origin
-git pull origin main
-git read-tree -mu HEAD
-
