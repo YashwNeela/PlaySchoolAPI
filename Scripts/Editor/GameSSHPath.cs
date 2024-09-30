56eea3694bfcac4576f7b82822cb5b8cf1ca18ba
YashwNeela <yashw@neelafilm.com>
2024-09-30 17:42:50 +0530

    added word game in game ssh

--- Scripts/Editor/GameSSHPath.cs
+++ Scripts/Editor/GameSSHPath.cs
@@ -18,11 +18,20 @@ public static class GameSSHPath
     
 
     #if UNITY_EDITOR_OSX
-        public static string spotTheDiffClone_SH = System.IO.Path.GetDirectoryName(Application.dataPath) + "/Assets/PlaySchoolAPI/ShellScripts/SpotTheDiff/SpotDifferenceClone_ios.sh";
+     public static string spotTheDiffClone_SH = System.IO.Path.GetDirectoryName(Application.dataPath) + "/Assets/PlaySchoolAPI/ShellScripts/SpotTheDiff/SpotDifferenceClone_ios.sh";
     public static string spotTheDiffPull_SH = System.IO.Path.GetDirectoryName(Application.dataPath) + "/Assets/PlaySchoolAPI/ShellScripts/SpotTheDiff/SpotDifferencePull_ios.sh";
     
     #elif UNITY_EDITOR
     public static string spotTheDiffClone_SH = System.IO.Path.GetDirectoryName(Application.dataPath) + "/Assets/PlaySchoolAPI/ShellScripts/SpotTheDiff/SpotDifferenceClone_android.sh";
     public static string spotTheDiffPull_SH = System.IO.Path.GetDirectoryName(Application.dataPath) + "/Assets/PlaySchoolAPI/ShellScripts/SpotTheDiff/SpotDifferencePull_android.sh";
+    #endif
+
+     #if UNITY_EDITOR_OSX
+     public static string wordGameClone_SH = System.IO.Path.GetDirectoryName(Application.dataPath) + "/Assets/PlaySchoolAPI/ShellScripts/WordGame/WordGameClone_ios.sh";
+    public static string wordGamePull_SH = System.IO.Path.GetDirectoryName(Application.dataPath) + "/Assets/PlaySchoolAPI/ShellScripts/WordGame/WordGamePull_ios.sh";
+    
+    #elif UNITY_EDITOR
+    public static string wordGameClone_SH = System.IO.Path.GetDirectoryName(Application.dataPath) + "/Assets/PlaySchoolAPI/ShellScripts/WordGame/WordGameClone_android.sh";
+    public static string wordGamePull_SH = System.IO.Path.GetDirectoryName(Application.dataPath) + "/Assets/PlaySchoolAPI/ShellScripts/WordGame/WordGamePull_android.sh";
     #endif
 }
